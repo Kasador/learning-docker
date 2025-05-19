e2145f404d3daba7bdb46f9ca557cf32f7fd0d22
Kasador <hunterstevenshaw@gmail.com>
2025-05-18 20:15:07 -0700

    FEAT: CRUD Ops good to go!

--- RESTful/routes/contacts.js
+++ RESTful/routes/contacts.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-   res.json({
-        success: true,
-        message: 'API is up and running - /api/v1/contacts',
-        method: `Method Used: ${req.method}`
-    })
-});
+// router.get('/', (req, res) => {
+//    res.json({
+//         success: true,
+//         message: 'API is up and running - /api/v1/contacts',
+//         method: `Method Used: ${req.method}`
+//     })
+// });
+
+const {
+  getAllContacts,
+  getContactById,
+  getUnpaginatedContacts,
+  createContact,
+  updateContact,
+  deleteContact
+} = require('../controllers/contactsController');
+
+// Routes
+router.get('/', getAllContacts); // /api/v1/contacts
+router.get('/all', getUnpaginatedContacts); // /api/v1/contacts/all
+router.get('/:id', getContactById); // /api/v1/contacts/:id
+router.post('/', createContact); // /api/v1/contacts
+router.put('/:id', updateContact); // /api/v1/contacts/:id
+router.delete('/:id', deleteContact); // /api/v1/contacts/:id
 
 module.exports = router;
